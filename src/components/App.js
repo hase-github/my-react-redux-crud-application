cc161bf89583daa49e13c2f74c36a7658b4dbdd5
hase-github <48000.10mai@gmail.com>
2021-04-30 13:26:21 +0900

    implement component with connent function

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,29 @@
-function App() {
-  return <h1>Hello. world</h1>;
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { increment, decrement } from "../actions";
+
+class App extends Component {
+  render() {
+    const props = this.props;
+    return (
+      <React.Fragment>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({ value: state.count.value });
+/*
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+});
+*/
+
+const mapDispatchToProps = { increment, decrement };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
