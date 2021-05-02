f40c914331e6b80f2f39504becfa711c8ee326e7
hase-github <48000.10mai@gmail.com>
2021-05-02 14:00:54 +0900

    implement events index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
-export const INCREMENT = "INCREMENT";
-export const DECREMENT = "DECREMENT";
+import axios from "axios";
+export const READ_EVENTS = "READ_EVENTS";
 
-//action creater
-export const increment = () => ({ type: INCREMENT });
+const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1";
+const QUERYSTRING = "?token=token123";
 
-export const decrement = () => ({ type: DECREMENT });
+//redux-thunkでACTIONの代わりに関数を返すaction createrを書くことができる
+export const readEvents = () => async (dispach) => {
+  //axiosは戻り値がPromiseになるので、async, awaitを使って戻り値を扱う
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`);
+  //console.log(response);
+  dispach({ type: READ_EVENTS, response });
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import EventsIndex from "./components/events_index";
 import reportWebVitals from "./reportWebVitals";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 
+import thunk from "redux-thunk";
+
 //アプリケーション内で唯一のstore
 //アプリケーションで作成されすstateはstoreに集約される
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 //他のコンポーネントからstoreを参照できるようにするために、
 //Providerでラップし、propsにstoreを渡す
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById("root")
 );
