4892dae060e00f3ec40a5facf23e3d79c2190723
hase-github <48000.10mai@gmail.com>
2021-05-02 14:42:31 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
 import reportWebVitals from "./reportWebVitals";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
-
 import thunk from "redux-thunk";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //アプリケーション内で唯一のstore
 //アプリケーションで作成されすstateはstoreに集約される
@@ -18,7 +19,12 @@ const store = createStore(reducer, applyMiddleware(thunk));
 //Providerでラップし、propsにstoreを渡す
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
